c10ad44986a0172b2e721c1968366314a50f5f61
casspc <casspc@gmail.com>
2014-10-07 10:30:47 -0700

    smarter script

--- createSymLinks.sh
+++ createSymLinks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ln -s -f /usr/local/krux/jdk1.7.0_67/bin/java /usr/local/bin/java7
-ln -s -f /usr/local/krux/jdk1.7.0_67/bin/javac /usr/local/bin/java7c
+ln -s -f /usr/local/oracle-java-7/bin/java /usr/local/bin/java7
+ln -s -f /usr/local/oracle-java-7/bin/javac /usr/local/bin/java7c
--- package.sh
+++ package.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-/usr/local/bin/fpm -s dir -t deb -n oracle-java-7 -a all -v 1.7.0-67 --after-install createSymLinks.sh --after-remove removeSymLinks.sh --prefix /usr/local/krux/ jdk1.7.0_67
+set -xe
+
+### The dir for the package script
+MY_DIR=$( dirname $0 )
+cd $MY_DIR
+
+### Name of the package, project, etc
+NAME=oracle-java-7
+
+### Packaging info
+PACKAGE_NAME=$NAME
+PACKAGE_VERSION="1.7.0-67"
+
+### Where this package will be installed
+DEST_DIR="/usr/local/${NAME}/"
+
+### Where the sources live
+SOURCE_DIR="${MY_DIR}/jdk1.7.0_67"
+
+/usr/local/bin/fpm -s dir -t deb -n $NAME -a all -v $PACKAGE_VERSION --after-install createSymLinks.sh --after-remove removeSymLinks.sh --prefix $DEST_DIR -C $SOURCE_DIR .
