2b078947ee7f6e6f79fe894d448c96b8c0a05784
Cass <casspc@gmail.com>
2014-10-07 09:41:56 -0700

    all architectures

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/fpm -s dir -t deb -n oracle-java-7 -v 1.7.0-67 --after-install createSymLinks.sh --after-remove removeSymLinks.sh --prefix /usr/local/krux/ jdk1.7.0_67
+/usr/local/bin/fpm -s dir -t deb -n oracle-java-7 -a all -v 1.7.0-67 --after-install createSymLinks.sh --after-remove removeSymLinks.sh --prefix /usr/local/krux/ jdk1.7.0_67
