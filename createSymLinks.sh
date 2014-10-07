64300c836610667561660f541ab1c6a525ccfcb0
casspc <casspc@gmail.com>
2014-10-06 22:31:59 -0700

    first add

---
+++ createSymLinks.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ln -s -f /usr/local/krux/jdk1.7.0_67/bin/java /usr/local/bin/java7
+ln -s -f /usr/local/krux/jdk1.7.0_67/bin/javac /usr/local/bin/java7c
---
+++ removeSymLinks.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -fr /usr/local/bin/java7
+rm -fr /usr/local/bin/java7c
